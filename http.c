fd9b0beb6f888e2fc26002654cb664aa1b1dcc6c
dingdang2012 <leonqin0101@gmail.com>
2016-03-28 16:41:03 -0700

    add parse url function

--- http.c
+++ http.c
@@ -1,5 +1,19 @@
-int parse_url(){
+/*parse hostname and path from url
+ * return 0 for success
+ * -1 for fail
+ * */
+int parse_url(char *url, char **host, char **path){
+  char *p = strstr(url, "//");
+  if(!p)
+    return -1;
 
-
-
+  *host = p + 2;
+  p = strchr(host, '/');
+  if(!p)
+    *path = NULL;
+  else{
+    *path = p + 1;
+    *p = '\0';
+  }
+  return 0;
 }
